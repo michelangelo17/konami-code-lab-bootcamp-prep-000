83f880927f92feafee8b2d645360e381462db228
Michelangelo Markus <michelangelo.markus@gmail.com>
2018-02-22 04:47:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 let index = 0;
 
 function init() {
-  document.addEventListener("keydown", codeCheck);
+  document.addEventListener("keydown", function(e) {
   
   function codeCheck(e) {
   const key = parseInt(e.detail || e.which);
@@ -20,5 +20,6 @@ function init() {
     index = 0;
   }
   }
+  });
 }
 
